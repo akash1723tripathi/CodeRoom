05a0c9877665c791c7f83bd91f1854f3a6403e82
akash1723tripathi <akashtripathi.acad@gmail.com>
2026-01-07 17:06:02 +0530

    Syncing Auth & Db

---
+++ backend/src/models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+
+  },
+  { timestamps: true } // createdAt, updatedAt
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,18 @@
 import express from "express"
+import cors from "cors"
+import { serve } from "inngest/express"
 import { ENV } from "./lib/env.js"
 import { connectDB }  from "./lib/db.js"
+import {inngest,functions} from "./lib/inngest.js"
 const app = express()
+
+//middleware
+app.use(express.json())
+// crendtials true allows to send cookies from frontend to backend
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
+
+app.use("api/inngest",serve({ client: inngest, functions }))
+
 
 app.get("/", (req, res) => {
       res.status(200).json({msg:"Success from API"})
